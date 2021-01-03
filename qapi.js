97168acf207c3bc8639de6afb54784c5ac508046
Ashish Dubey <ashishdubey290@gmail.com>
2021-01-03 12:33:20 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ff16bb96d6deb001f919374",
+				"5ff16bb96d6deb001f919376"
+			]
+		},
+		"5ff16bb96d6deb001f919374": {
+			"id": "5ff16bb96d6deb001f919374",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ff16bb96d6deb001f919376": {
+			"id": "5ff16bb96d6deb001f919376",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
